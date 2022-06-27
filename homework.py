bc9c01db1a12c8af944498dfa8f025270ec2ad76
Евгений Балуев <baluew.1998@yandex.ru>
2022-06-27 22:54:16 +0300

    Second commit

--- homework.py
+++ homework.py
@@ -11,14 +11,14 @@ class InfoMessage:
         self.distance = distance
         self.speed = speed
         self.calories = calories
-      
+
     def get_message(self):
         return (f"Тип тренировки: {self.training_type}; "
                 f"Длительность: {self.duration:.3f} ч.; "
                 f"Дистанция: {self.distance:.3f} км; "
                 f"Ср. скорость: {self.speed:.3f} км/ч; "
                 f"Потрачено ккал: {self.calories:.3f}.")
-    
+
 
 class Training:
     """Базовый класс тренировки."""
@@ -49,7 +49,7 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(self.__class__.__name__, self.duration, 
+        return InfoMessage(self.__class__.__name__, self.duration,
                            self.get_distance(), self.get_mean_speed(),
                            self.get_spent_calories())
 
@@ -58,13 +58,13 @@ class Running(Training):
     """Тренировка: бег."""
     def __init__(self, action: int, duration: float, weight: float):
         super().__init__(action, duration, weight)
-    
+
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий для RUN."""
         COEFF_COLORE_1: int = 18
         COEFF_COLORE_2: int = 20
-        return((COEFF_COLORE_1 * self.get_mean_speed() - COEFF_COLORE_2) *
-               self.weight / self.M_IN_KM * self.duration * self.MIN_IN_HOUR)
+        return((COEFF_COLORE_1 * self.get_mean_speed() - COEFF_COLORE_2)
+               * self.weight / self.M_IN_KM * self.duration * self.MIN_IN_HOUR)
 
 
 class SportsWalking(Training):
@@ -73,14 +73,14 @@ class SportsWalking(Training):
                  weight: float, height: float):
         super().__init__(action, duration, weight)
         self.height = height
-    
+
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий для WLK."""
         COEFF_COLORE_3: int = 0.035
         COEFF_COLORE_4: int = 0.029
-        return((COEFF_COLORE_3 * self.weight +
-               (self.get_mean_speed()**2 // self.height) * COEFF_COLORE_4 *
-                self.weight) * self.duration * self.MIN_IN_HOUR)
+        return((COEFF_COLORE_3 * self.weight
+               + (self.get_mean_speed()**2 // self.height) * COEFF_COLORE_4
+               * self.weight) * self.duration * self.MIN_IN_HOUR)
 
 
 class Swimming(Training):
@@ -91,25 +91,25 @@ class Swimming(Training):
         super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
-    
+
     LEN_STEP: float = 1.38
-    
+
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
         return self.action * self.LEN_STEP / self.M_IN_KM
-    
+
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения для SWM."""
-        return (self.length_pool * self.count_pool 
+        return (self.length_pool * self.count_pool
                 / self.M_IN_KM) / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий для SWM"""
         COEFF_COLORE_5 = 1.1
         COEFF_COLORE_6 = 2
-        return(self.get_mean_speed() +
-               COEFF_COLORE_5) * COEFF_COLORE_6 * self.weight
-        
+        return(self.get_mean_speed()
+               + COEFF_COLORE_5) * COEFF_COLORE_6 * self.weight
+
 
 def read_package(workout_type: str, data: list[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
@@ -124,7 +124,7 @@ def main(training: Training) -> None:
     """Главная функция."""
     info = training.show_training_info()
     print(info.get_message())
-    
+
 
 if __name__ == '__main__':
     packages = [
@@ -136,4 +136,3 @@ if __name__ == '__main__':
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
-
